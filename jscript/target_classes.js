9169107df04fd3e52fa60925a21856fad439c21b
vbookshelf <44088528+vbookshelf@users.noreply.github.com>
2018-12-14 22:39:39 +0200

    added files

---
+++ jscript/target_classes.js
@@ -0,0 +1,18 @@
+
+
+TARGET_CLASSES = {
+  0: "Black-grass (Weed)",
+  1: "Charlock (Weed)",
+  2: "Cleavers (Weed)",
+  3: "Common Chickweed (Weed)",
+  4: "Common wheat",
+  5: "Fat Hen (Weed)",
+  6: "Loose Silky-bent (Weed)",
+  7: "Maize",
+  8: "Scentless Mayweed (Weed)",
+  9: "Shepherd’s Purse (Weed)",
+  10: "Small-flowered Cranesbill (Weed)",
+  11: "Sugar beet"
+
+
+};
